bca742ea70ec78ccce4eb8250e7ddebbc3bbf987
Kevin Lyda <kevin@lyda.ie>
2020-02-23 19:01:17 +0000

    :construction_worker: Seeded dokuwiki

---
+++ t/dokuwiki/files/var/www/dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+doku:$1$DXUjWvuH$8Tr1ypb3oDlvftDGKb712.:Doku Wiki:test@example.com:admin,user
