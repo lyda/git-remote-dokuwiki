fda6f266f89b211d0b1a9ff134a5f8559d098fb6
Kevin Lyda <kevin@lyda.ie>
2020-02-23 16:31:42 +0000

    :art: More project structure changes.

---
+++ lib/App/Git/Remote/Dokuwiki.pm
@@ -0,0 +1,72 @@
+package lib::App::Git::Remote::Dokuwiki;
+
+# Copyright (C) 2020
+#     Kevin Lyda <kevin@lyda.ie>
+# License: GPL v2 or later
+
+=head1 NAME
+
+App::Git::Remote::Dokuwiki - A git remote for dokuwiki
+
+=head1 VERSION
+
+Version 0.01
+=cut
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+This script allows git to clone C<dokuwiki::> urls.
+
+=head1 DESCRIPTION
+
+This script is intended to be executed by git.  To use it put it in your
+PATH and clone a dokuwiki like so:
+
+    git clone dokuwiki::http://dokuwiki.example.com/
+
+You can pass certain configuration parameters at clone time.
+
+=over
+
+=item C<-c remote.REMOTE_NAME.dwUser=USER> - the Dokuwiki user name
+
+=item C<-c remote.REMOTE_NAME.dwPassword=PASSWORD> - the Dokuwiki password
+
+=back
+
+All commits are pulled onto the master branch.  Dokuwiki has no concept
+of branches so no other branches will be managed.  You can, of course,
+push those branches to other git remotes.
+
+Push is not yet supported.
+
+Attachments have the executable bit set.  Pages do not.  This will matter
+for pushes.
+
+=head1 LINKS
+
+The following links were very useful in writing this:
+
+=over
+
+=item L<Git::Mediawiki|https://github.com/Git-Mediawiki/Git-Mediawiki> project
+
+=item The Dokuwiki L<xmlrpc docs|https://www.dokuwiki.org/devel:xmlrpc:clients>
+
+=back
+
+=head1 AUTHOR
+
+Kevin Lyda <kevin@lyda.ie>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2020 Kevin Lyda.
+
+=head1 LICENSE
+
+This script is free software and is distributed under the GPLv2; see the
+LICENSE file for more information.
+
+=cut
